1a85c51a91082d46e3f8324d0fee0ccf6e2246c3
Yuguo Zhao <cheerzhao77@gmail.com>
2021-02-07 15:09:17 -0600

    refactor:add reducers folder

---
+++ src/reducers/todos.js
@@ -0,0 +1,31 @@
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  TOGGLE_TODO
+} from "../actions/todos"
+
+import {
+  RECEIVE_DATA
+} from "../actions/shared";
+
+export default function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+      break;
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+      break;
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    case RECEIVE_DATA:
+      return action.todos;
+      break;
+    default:
+      return state;
+  }
+}
